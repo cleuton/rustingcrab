8fd6a2cfea2218b845239707e79e9650bf97bfea
cleutonsampaio <cleuton.sampaio@gmail.com>
2024-12-12 11:55:32 -0300

    game-2

--- code_samples/game/ferris_hunter/src/main.rs
+++ code_samples/game/ferris_hunter/src/main.rs
@@ -1,8 +1,10 @@
+/* Game template para usar ggez - Cleuton Sampaio*/
+
 use ggez::{Context, ContextBuilder, GameResult};
 use ggez::graphics::{self, Color};
 use ggez::event::{self, EventHandler};
 use ggez::conf::{WindowSetup, WindowMode};
-use ggez::winit::dpi::{LogicalPosition, PhysicalPosition};
+use ggez::winit::dpi::PhysicalPosition;
 use rdev::{display_size};
 
 fn main() {
@@ -14,48 +16,47 @@ fn main() {
     let pos_x = ((largura as f32) - 1024.0) / 2.0;
     let pos_y = ((altura as f32)  - 768.0) / 2.0;
 
-    // Make a Context.
-    let (mut ctx, event_loop) = ContextBuilder::new("ferris_hunter", "Cleuton Sampaio")
+    // Criar um contexto
+    let (mut ctx, loop_eventos) = ContextBuilder::new("ferris_hunter", "Cleuton Sampaio")
         .window_setup(WindowSetup::default().title("Ferrus Hunter!"))
         .window_mode(WindowMode::default().dimensions(1024.0, 768.0))
         .build()
         .expect("aieee, could not create ggez context!");
 
-    let mut context = &ctx.gfx;
+    // Posiciona a janela do jogo:no centro do monitor
+    let context = &ctx.gfx;
     let pos = PhysicalPosition::new(pos_x as i32, pos_y as i32);
-    context.set_window_position(pos);
+    context.set_window_position(pos).unwrap();
 
-    // Create an instance of your event handler.
-    // Usually, you should provide it with the Context object to
-    // use when setting your game up.
-    let my_game = MyGame::new(&mut ctx);
+    // Cria uma instância do jogo
+    let jogo = Jogo::new(&mut ctx);
 
-    // Run!
-    event::run(ctx, event_loop, my_game);
+    // Inicia o loop de eventos do jogo
+    event::run(ctx, loop_eventos, jogo);
 }
 
-struct MyGame {
-    // Your state here...
+struct Jogo {
+    // Aqui você define o estado do jogo: Posições, velocidades, etc.
 }
 
-impl MyGame {
-    pub fn new(_ctx: &mut Context) -> MyGame {
-        // Load/create resources such as images here.
-        MyGame {
+impl Jogo {
+    pub fn new(_ctx: &mut Context) -> Jogo {
+        // Carregue / crie recursos como imagens aqui.
+        Jogo {
             // ...
         }
     }
 }
 
-impl EventHandler for MyGame {
+impl EventHandler for Jogo {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        // Update code here...
+        // Código para atualizar os gameobjects...
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
-        // Draw code here...
+        let canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
+        // Código para desenhar os gameobjects...
         canvas.finish(ctx)
     }
 }
